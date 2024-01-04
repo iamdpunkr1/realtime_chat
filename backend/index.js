795feb6bf846911b9664e30a9a601c207438d263
Dipankar Prasad <iamdpunkr@gmail.com>
2024-01-05 00:41:23 +0530

    feat: add ChatEngine API

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+    
+    const { username } = req.body;
+    console.log(username);
+  try{
+    const r = await axios.put("https://api.chatengine.io/users/",
+    {username, secret: username, first_name: username},
+    {headers: {"Private-Key": process.env.chat_engine_private_key}
+    });
+
+    return res.status(r.status).json(r.data)
+
+  }catch(e){
+    
+    return res.status(e.response.status).json(e.response.data);
+  }
+  
 });
 
 app.listen(3001,()=> console.log("Server started on port 3001"));
